cc380c9f933d6515cce53bcb04bbd92beacdc33d
Ian Polding <ipolding@gmail.com>
2014-05-27 14:36:52 +0100

    moved add marker to ctrl

--- js/controllers.js
+++ js/controllers.js
@@ -5,9 +5,15 @@ whsMapper.controller('MapCtrl', ['$scope',
 	function ($scope) {
 		$scope.site = 'Budapest';
 
-
-	}
-
-
+		$scope.addMarker = function() {
+			var myLatlng = new google.maps.LatLng(lat, longitude);
+  			var marker = new google.maps.Marker({
+        	
+        	position: myLatlng,
+        	map: map,
+        	title:"Hello World!"});
+		}
+			
+		}
 
 	]);
