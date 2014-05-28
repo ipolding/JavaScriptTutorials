462563ba1cb627662abfb1838ad030386003662a
ian_polding <ian.polding@wowcher.co.uk>
2014-05-28 10:54:21 +0100

    Angular form directives and controller now adds marker

--- js/controllers.js
+++ js/controllers.js
@@ -4,9 +4,11 @@ whsMapper.controller('MapCtrl', ['$scope',
 
 	function ($scope) {
 		$scope.site = 'Budapest';
+		$scope.latitude;
+		$scope.longitude;
 
 		$scope.addMarker = function() {
-			var myLatlng = new google.maps.LatLng(lat, longitude);
+			var myLatlng = new google.maps.LatLng($scope.latitude, $scope.longitude);
   			var marker = new google.maps.Marker({
         	
         	position: myLatlng,
