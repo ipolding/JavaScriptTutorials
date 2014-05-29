0ae71d8d7f0ba54e251e480650b0660a03b2a00f
ian_polding <ian.polding@wowcher.co.uk>
2014-05-29 17:23:24 +0100

    placing markers on map is working

--- js/controllers.js
+++ js/controllers.js
@@ -1,5 +1,9 @@
 var whsMapper = angular.module('whsMapper', ['ngResource']);
 
+var alerter = function() {
+	window.alert("Alert!");
+}
+
 whsMapper.controller('MapCtrl', ['$scope', '$resource',  
 
 	function ($scope, $resource) {
@@ -11,14 +15,24 @@ whsMapper.controller('MapCtrl', ['$scope', '$resource',
 		$scope.latitude;
 		$scope.longitude;
 
-		$scope.addMarkerManually = function() {
-			var myLatlng = new google.maps.LatLng($scope.latitude, $scope.longitude);
+		$scope.addMarker = function(siteLatitude, siteLongitude) {
+			var myLatlng = new google.maps.LatLng(siteLatitude, siteLongitude);
   			var marker = new google.maps.Marker({
         	
         	position: myLatlng,
         	map: map,
         	title:"Hello World!"});
-		}
+
+		};
+
+		// $scope.addMarkerManually = function() {
+		// 	var myLatlng = new google.maps.LatLng($scope.latitude, $scope.longitude);
+  // 			var marker = new google.maps.Marker({
+        	
+  //       	position: myLatlng,
+  //       	map: map,
+  //       	title:"Hello World!"});
+		// };
 			
 		}
 
